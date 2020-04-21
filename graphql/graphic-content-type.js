03137d0e8d17694bf8568d64dc8be6a61e788f4e
Terri Regan <contact@terriregan.com>
2020-04-21 16:12:19 -0400

    Moved graphql mocks to this repo

---
+++ graphql/graphic-content-type.js
@@ -0,0 +1,241 @@
+export const full = {
+  id: 'ck8en7r8x0b7007652jpf9a59',
+  createdAt: '2020-03-30T15:44:20.145Z',
+  updatedAt: '2020-04-13T14:54:40.647Z',
+  publishedAt: '2020-03-30T15:44:51.511Z',
+  title: 'Coffee Growers',
+  type: 'SOCIAL_MEDIA',
+  copyright: 'GPA © 2020',
+  alt: '',
+  descPublic: 'Public description',
+  descInternal: 'Internal description',
+  assetPath: 'graphic/2020/03/commons.america.gov_ck8enbkxs0bdy076501iy0akv',
+  status: 'PUBLISHED',
+  visibility: 'PUBLIC',
+  author: {
+    id: 'ck8embiwq0b1n0765ps892v3n',
+    email: 'reganta@america.gov',
+    firstName: 'Terri',
+    lastName: 'Regan',
+    jobTitle: null,
+    country: null,
+    city: null,
+    howHeard: null,
+    permissions: ['ADMIN'],
+    team: {
+      id: 'ck8em6gwa004p0765q6mjvh5f',
+      name: 'GPA Video',
+      organization: 'Department of State',
+      members: null,
+      contentTypes: ['VIDEO'],
+      isConfirmed: true
+    }
+  },
+  team: {
+    id: 'ck8em6gwa004p0765q6mjvh5f',
+    name: 'GPA Video',
+    organization: 'Department of State',
+    members: null,
+    contentTypes: ['VIDEO'],
+    isConfirmed: true
+  },
+  images: [
+    {
+      id: 'ck8en85go0b820765gcv0kgrx',
+      createdAt: '2020-03-30T15:44:38.502Z',
+      updatedAt: '2020-03-30T15:44:38.502Z',
+      visibility: 'PUBLIC',
+      language: {
+        id: 'ck8em6gw9004n07653maurqwc',
+        locale: 'en-us',
+        languageCode: 'en',
+        textDirection: 'LTR',
+        displayName: 'English',
+        nativeName: 'English'
+      },
+      alt: 'Will be populated from project level',
+      url: '2020/03/commons.america.gov_ck8en7r8x0b7007652jpf9a59/CAPTIONS_Made_in_America_ENGLISH_Output.srt',
+      signedUrl: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/03/commons.america.gov_ck8en7r8x0b7007652jpf9a59/CAPTIONS_Made_in_America_ENGLISH_Output.srt?AWSAccessKeyId=AKIAJ7VASWQMY6B7CH7A&Expires=1587475657&Signature=ONFhAwXKyGmKQ4tzZb%2FxA8rN6m8%3D',
+      filename: 'coffee.jpg',
+      filetype: 'image/jpeg',
+      filesize: 3193,
+      style: 'Clean',
+      social: 'Twitter'
+    }
+  ],
+  supportFiles: [
+    {
+      id: 'ck8en85go0b820765gcv0kgrx',
+      createdAt: '2020-03-30T15:44:38.502Z',
+      updatedAt: '2020-03-30T15:44:38.502Z',
+      language: {
+        id: 'ck8em6gw9004n07653maurqwc',
+        locale: 'en-us',
+        languageCode: 'en',
+        textDirection: 'LTR',
+        displayName: 'English',
+        nativeName: 'English'
+      },
+      url: '2020/03/commons.america.gov_ck8en7r8x0b7007652jpf9a59/CAPTIONS_Made_in_America_ENGLISH_Output.srt',
+      signedUrl: 'https://amgov-publisher-dev.s3.amazonaws.com/2020/03/commons.america.gov_ck8en7r8x0b7007652jpf9a59/CAPTIONS_Made_in_America_ENGLISH_Output.srt?AWSAccessKeyId=AKIAJ7VASWQMY6B7CH7A&Expires=1587475657&Signature=ONFhAwXKyGmKQ4tzZb%2FxA8rN6m8%3D',
+      filename: 'CAPTIONS Made in America ENGLISH_Output.srt',
+      filetype: 'application/x-subrip',
+      filesize: 3193,
+      visibility: 'PUBLIC'
+    }
+  ],
+  categories: [
+    {
+      id: 'ck8em77tg0arm0765sgq55glk',
+      translations: [
+        {
+          id: 'ck8em6j7z01cq0765t0q5d7hh',
+          name: 'global issues',
+          language: {
+            id: 'ck8em6gw9004n07653maurqwc'
+          }
+        },
+        {
+          id: 'ck8em6j8s01cx0765zhy1517b',
+          name: 'Asuntos mundiales',
+          language: {
+            id: 'ck8em6gyq005x0765tf5gfcdo'
+          }
+        },
+        {
+          id: 'ck8em6j9k01d40765nhcjpfci',
+          name: 'Questions mondiales',
+          language: {
+            id: 'ck8em6gwb004q0765aeozhhrx'
+          }
+        },
+        {
+          id: 'ck8em6jaf01db07651bfmjm9c',
+          name: 'Questões globais',
+          language: {
+            id: 'ck8em6gy4005g076525h11w7s'
+          }
+        },
+        {
+          id: 'ck8em6jbb01di0765wsws9vxi',
+          name: 'قضايا عالمية',
+          language: {
+            id: 'ck8em6gwh004s0765zz4sk01a'
+          }
+        },
+        {
+          id: 'ck8em6jc501dp0765jhpifkh2',
+          name: 'مسائل جهانی',
+          language: {
+            id: 'ck8em6gy6005k0765hpstlk8b'
+          }
+        },
+        {
+          id: 'ck8em6jcz01dw0765xm5cf5w8',
+          name: '全球事务',
+          language: {
+            id: 'ck8em6gwa004o0765ggxu56er'
+          }
+        },
+        {
+          id: 'ck8em6jdx01e307655cspggta',
+          name: 'Глобальные вопросы',
+          language: {
+            id: 'ck8em6gyf005r0765w761manj'
+          }
+        },
+        {
+          id: 'ck8em6jep01ea07651tyz063v',
+          name: 'Isu Global',
+          language: {
+            id: 'ck8em6gx5004v0765lyp07s9x'
+          }
+        },
+        {
+          id: 'ck8em6jfi01eh076513obxnpz',
+          name: 'عالمگیر مسائل',
+          language: {
+            id: 'ck8em6gyn005t0765zgetgcsq'
+          }
+        }
+      ]
+    }
+  ],
+  tags: [
+    {
+      id: 'ck8em77tk0at60765lecud41v',
+      translations: [
+        {
+          id: 'ck8em6t7j04dg0765hwrdmh5h',
+          name: 'agriculture',
+          language: {
+            id: 'ck8em6gw9004n07653maurqwc'
+          }
+        },
+        {
+          id: 'ck8em6t8304dn0765nhrhysfo',
+          name: 'agricultura',
+          language: {
+            id: 'ck8em6gyq005x0765tf5gfcdo'
+          }
+        },
+        {
+          id: 'ck8em6t8o04du0765ycoeq4sj',
+          name: 'agriculture',
+          language: {
+            id: 'ck8em6gwb004q0765aeozhhrx'
+          }
+        },
+        {
+          id: 'ck8em6t9704e10765q0s3msnp',
+          name: 'agricultura',
+          language: {
+            id: 'ck8em6gy4005g076525h11w7s'
+          }
+        },
+        {
+          id: 'ck8em6t9s04e80765085ydhcm',
+          name: 'الزراعة',
+          language: {
+            id: 'ck8em6gwh004s0765zz4sk01a'
+          }
+        },
+        {
+          id: 'ck8em6tad04ef0765mifg0q25',
+          name: 'کشاورزی',
+          language: {
+            id: 'ck8em6gy6005k0765hpstlk8b'
+          }
+        },
+        {
+          id: 'ck8em6tax04em0765ho5t9f45',
+          name: '農業',
+          language: {
+            id: 'ck8em6gwa004o0765ggxu56er'
+          }
+        },
+        {
+          id: 'ck8em6tbh04et0765lea4bab4',
+          name: 'сельское хозяйство',
+          language: {
+            id: 'ck8em6gyf005r0765w761manj'
+          }
+        },
+        {
+          id: 'ck8em6tc104f00765jab19apr',
+          name: 'pertanian',
+          language: {
+            id: 'ck8em6gx5004v0765lyp07s9x'
+          }
+        },
+        {
+          id: 'ck8em6tcs04f70765xcnl74gx',
+          name: 'زراعت',
+          language: {
+            id: 'ck8em6gyn005t0765zgetgcsq'
+          }
+        }
+      ]
+    }
+  ]
+};
